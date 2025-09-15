30800a84cfeda32a02b3d6439b6a8fdfc0740880
oliverfaris <oliver.faris@menloschool.org>
2025-09-14 23:58:13 -0700

    V1 worked in work log mostly

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Oliver Faris
  *
  */
 
@@ -15,6 +17,19 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        // If hospitals are cheaper or the same cost as a highway, it's ultimately cheaper to have a hospital in every city
+        if (hospitalCost <= highwayCost) {
+            return (long) n *hospitalCost;
+        }
+        int minimumCost = 0;
+        // ArrayList<Integer> numGroups = new ArrayList<>();
+        int[][] numGroups = new int[4][4];
+        for (int i=0; i < cities.length; i++) {
+            if (cities[i][0] == 0) {
+
+            }
+        }
+
         return 0;
     }
 }
